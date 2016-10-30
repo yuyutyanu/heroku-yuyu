c2ae80b6f10125155843edb9a19163acc0960840
yuyutyanu <yuyutyanu@yahoo.co.jp>
2016-10-30 12:51:12 +0900

    huwahuwa obj

---
+++ resources/views/huwahuwa.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>huwahuwa</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="box1">
+
+      </div>
+    </div>
+
+    <style media="screen">
+      .box1{
+      border: solid 1px;
+      }
+      @keyframes .box1 {
+        0% {width: 100px; height: 50px; background-color: aqua; position: absolute; bottom:0px;}
+        100% {width: 100px; height: 50px; background-color: blue; position: absolute; bottom:50px;}
+      }
+    </style>
+  </body>
+</html>
