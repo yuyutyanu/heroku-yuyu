c9d70974896c650e7cf991b36aa5c3caaeda1e68
yuyutyanu <yuyutyanu@yahoo.co.jp>
2016-10-30 13:00:12 +0900

    huwahuwa obj

--- resources/views/huwahuwa.blade.php
+++ resources/views/huwahuwa.blade.php
@@ -13,11 +13,29 @@
 
     <style media="screen">
       .box1{
-      animation: anime1 5s ease -2s infinite alternate;
+      border-radius: 50% 50% 50% 50%;
+      bottom:0px;
+      top:0px;
+      left:0px;
+      right:0px;
+      margin:auto;
+      animation: anime1 1s ease -2s infinite alternate;
       }
       @keyframes anime1 {
-        0% {width: 100px; height: 50px; background-color: aqua; position: absolute; bottom:0px;border: solid 1px;}
-        100% {width: 100px; height: 50px; background-color: blue; position: absolute; bottom:50px; border:solid 1px;}
+        0% {width: 100px;
+           height: 50px;
+           background-color: aqua;
+           position: absolute;
+           border: solid 1px;
+      }
+        100% {
+           width: 100px;
+           height: 50px;
+           background-color: blue;
+           position: absolute;
+           border:solid 1px;
+           margin-bottom: 50px;
+           }
       }
     </style>
   </body>
